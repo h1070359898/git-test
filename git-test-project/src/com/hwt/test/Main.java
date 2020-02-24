1ffb36c7023de062da7f933bcf9e340c1658bd4f
hwt <1070359898@qq.com>
2020-02-24 17:17:28 +0800

    修改

---
+++ git-test-project/src/com/hwt/test/Main.java
@@ -0,0 +1,14 @@
+package com.hwt.test;
+
+public class Main {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+        System.out.println("测试修改");
+        test();
+    }
+    static void test(){
+        int i = 0;
+        System.out.println(i);
+    }
+}
